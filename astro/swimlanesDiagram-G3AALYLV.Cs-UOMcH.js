afcc8aa94b4c92f1d7ac46023769042d486de91a
icealtria <icealtria+git@gmail.com>
2026-09-09 12:37:38 +0000

    Update from https://github.com/icealtria/blog/commit/7140702542e59daea21aff1432a45463bd8cc6b9

--- astro/swimlanesDiagram-G3AALYLV.Cs-UOMcH.js
+++
@@ -1,8 +0,0 @@
-import"./src.OgxTg4hg.js";import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./chunk-WYO6CB5R.I_nvq0QK.js";import"./chunk-ICXQ74PX.Cw-K2hmQ.js";import"./chunk-HOUHSVGY.D0170Zue.js";import"./chunk-Q4XR5HBZ.BdGrzxyz.js";import"./chunk-7BUUIJ7U.CAhmNhj7.js";import"./chunk-OGEWGWER.U3PprAZN.js";import"./chunk-32BRIVSS.Bu3PzTrs.js";import"./chunk-XXDRQBXY.B0iYp7hA.js";import"./chunk-VR4S4FIN.CLlnXBIr.js";import"./chunk-C7G6YPKG.BtJt1eHH.js";import"./chunk-ZGVPDNZ5.CAkHDW-L.js";import"./chunk-52WLFC77.DDOtTBka.js";import"./chunk-FWX5IMBZ.k6s2Bh83.js";import"./chunk-ZIRB5QZD.Dh20_f-h.js";import{r as t,t as n}from"./chunk-PUDLZKDR.CNhPWFDB.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
